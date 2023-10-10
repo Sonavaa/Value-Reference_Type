4529560ef6e9f6683132bb016c8e7c69bde7c803
Sonava <sonava@code.edu.az>
2023-10-10 22:54:54 +0400

    Task tamamlandı

---
+++ Value&Referance_Type/Program.cs
@@ -0,0 +1,23 @@
+﻿//Substring methodunu custom yazmaq.
+
+//string Array =  "Salam necesiz " ;
+//Console.WriteLine(  Array.Substring(6));
+
+string Word=Console.ReadLine();
+string index=Console.ReadLine();
+int Index = int .Parse(index);
+string length = Console.ReadLine();
+int Length=int .Parse(length);
+
+char[] smt = { };
+Substring(Word,Index,Length);
+
+void Substring(string word, int i, int length)
+{
+        char[] Letters = word.ToCharArray();
+      for (int j = i; j <= length; j++)
+    {
+        smt = smt.Append(Letters[j]).ToArray();  
+    }
+    Console.WriteLine(smt);
+}
